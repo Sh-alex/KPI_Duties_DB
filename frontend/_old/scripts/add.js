2d104b4056b4b8a6afac4c997b5a4537bb8c23d7
MaxGenash <123maks189@mail.ru>
2016-07-11 22:30:56 +0300

    Upgraded markup of add page Added markup of modal-window for adding info from related occupations

--- frontend/_old/scripts/add.js
+++ frontend/_old/scripts/add.js
@@ -1,3 +1,36 @@
-/**
- * Created by MaxGenash on 24.05.2016.
- */
+$(".inp-name-block").on("change", '[name="radio-type-of-occup-name-inp"]', function (e) {
+    let radioBtn = $(e.target),
+        classNameMltiselect = "inp-name-block--radio-uses-multiselect",
+        classNameInput = "inp-name-block--radio-uses-input",
+        inpNameBlock = radioBtn.closest(".inp-name-block")   ;
+    if(radioBtn.hasClass(classNameMltiselect)) {
+        inpNameBlock
+            .removeClass(classNameInput)
+            .addClass(classNameMltiselect);
+    } else {
+        inpNameBlock
+            .removeClass(classNameMltiselect)
+            .addClass(classNameInput);
+    }
+});
+
+$(".form-search-similar").on("submit", function (e) {
+    e.preventDefault();
+    let modal = $(this).closest(".modal-add-info-from-related"),
+        classSearchForm = "modal-add-info-from-related--search-form",
+        classSearchResults = "modal-add-info-from-related--search-results";
+
+    modal
+        .removeClass(classSearchForm)
+        .addClass(classSearchResults);
+});
+
+$(".btn-back-to-search-form").on("click", function (e) {
+    let modal = $(this).closest(".modal-add-info-from-related"),
+        classSearchForm = "modal-add-info-from-related--search-form",
+        classSearchResults = "modal-add-info-from-related--search-results";
+
+        modal
+            .removeClass(classSearchResults)
+            .addClass(classSearchForm);
+});
