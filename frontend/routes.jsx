73d9f54d3557101e7e90d0f25b3dd40274fa2cdf
MaxGenash <123maks189@mail.ru>
2016-08-07 21:20:46 +0300

    added frontend routes

--- frontend/routes.jsx
+++ frontend/routes.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import App from './containers/App'
+import NotFound from './components/NotFound'
+
+/*
 import Admin from './components/Admin'
 import List from './components/List'
 import Genre from './components/Genre'
 import Release from './components/Release'
 import Home from './components/Home'
 import LoginPage from './containers/LoginPage'
-import NotFound from './components/NotFound'
 import requireAuthentication from './containers/AuthenticatedComponent'
 
 export const routes = (
@@ -25,3 +26,25 @@ export const routes = (
     <Route path='*' component={NotFound} />
   </div>
 )
+*/
+
+import App from './containers/App'
+import LoginBox from './components/LoginBox'
+import SearchFormBox from './components/SearchFormBox'
+import SearchResultsBox from './components/SearchResultsBox'
+import AddOccupBox from './components/AddOccupBox'
+import MainLayout from './components/MainLayout'
+
+export const routes = (
+    <div>
+        <Route path='/' component={App}>
+            <IndexRoute component={LoginBox} />
+            <Route component={MainLayout}>
+                <Route path="add" component={AddOccupBox} />
+                <Route path="search" component={SearchFormBox} />
+                <Route path="search?:searchParams" components={{SearchFormBox, SearchResultsBox}} />
+            </Route>
+        </Route>
+        <Route path='*' component={NotFound} />
+    </div>
+);
