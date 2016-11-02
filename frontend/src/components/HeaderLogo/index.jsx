0625098f0fb87ed22a2ba6863e08b267dbbbc59a
MaxGenash <123maks189@mail.ru>
2016-11-02 09:24:04 +0200

    Changed name of the web-app

--- frontend/src/components/HeaderLogo/index.jsx
+++ frontend/src/components/HeaderLogo/index.jsx
@@ -5,11 +5,8 @@ import "./styles.less";
 export default class HeaderLogo extends Component {
   render() {
     return <Link to='/' className="header-logo">
-        <p className="header-logo-text header-logo-text--full hidden">
-            Державний<br />класифікатор<br />посад<br />України
-        </p>
         <p className="header-logo-text header-logo-text--abbr">
-            ДКПУ
+            АІС "КС"
         </p>
     </Link>
   }
