e44c93ad883f610cf33c48468a8648a12700b143
GalchenkoYaroslav <yarosgalchenko@gmail.com>
2016-11-28 17:14:51 +0200

    authnew

--- src/main/java/com/kpi/kpi_duties_db/config/DatabaseConfig.java
+++ src/main/java/com/kpi/kpi_duties_db/config/DatabaseConfig.java
@@ -2,152 +2,19 @@ package com.kpi.kpi_duties_db.config;
 
 
 import org.hibernate.SessionFactory;
-import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import javax.sql.DataSource;
 import java.util.Properties;
-//
-//
-//@Configuration
-//@EnableTransactionManagement
-//@EnableJpaRepositories("com.kpi.kpi_duties_db.repository")
-//@PropertySource("classpath:application.properties")
-//public class DatabaseConfig {
-//
-//    private static final String PROP_DATABASE_DRIVER = "db.driver";
-//    private static final String PROP_DATABASE_PASSWORD = "db.password";
-//    private static final String PROP_DATABASE_URL = "db.url";
-//    private static final String PROP_DATABASE_USERNAME = "db.username";
-//    private static final String PROP_HIBERNATE_DIALECT = "db.hibernate.dialect";
-//    private static final String PROP_HIBERNATE_SHOW_SQL = "db.hibernate.show_sql";
-//    private static final String PROP_ENTITYMANAGER_PACKAGES_TO_SCAN = "db.entitymanager.packages.to.scan";
-//    private static final String PROP_HIBERNATE_HBM2DDL_AUTO = "db.hibernate.hbm2ddl.auto";
-//
-//
-//    @Autowired
-//    private Environment env;
-//
-//
-////    @Bean
-////    CommandLineRunner init(StorageService storageService) {
-////        return (args) -> {
-////            storageService.deleteAll();
-////            storageService.init();
-////        };
-////    }
-//
-////    @Bean
-////    public DetailService detailService(){
-////        DetailService detailService = new DetailServiceImpl();
-////        return detailService;
-////    }
-//
-//
-//    @Bean
-//    public LocalSessionFactoryBean sessionFactoryBean() {
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//        sessionFactory.setDataSource(dataSource());
-//        sessionFactory.setPackagesToScan("com.kpi.kpi_duties_db.domain");
-//        sessionFactory.setHibernateProperties(getHibernateProperties());
-//        return sessionFactory;
-//    }
-//
-//
-//
-////    @Bean
-////    public DetailService detailsService(){
-////        return new DetailServiceImpl();
-////    }
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//
-//        dataSource.setDriverClassName(env.getProperty(PROP_DATABASE_DRIVER));
-//        dataSource.setUrl(env.getProperty(PROP_DATABASE_URL));
-//        dataSource.setUsername(env.getProperty(PROP_DATABASE_USERNAME));
-//        dataSource.setPassword(env.getProperty(PROP_DATABASE_PASSWORD));
-//
-//        return dataSource;
-//    }
-//
-//
-//    @Bean
-//    public SessionFactory sessionFactory() {
-//        return sessionFactoryBean().getObject();
-//    }
-//
-//
-//
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-//        entityManagerFactoryBean.setDataSource(dataSource());
-//        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-//        entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(PROP_ENTITYMANAGER_PACKAGES_TO_SCAN));
-//
-//        entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
-//
-//        return entityManagerFactoryBean;
-//    }
-//
-//    @Bean
-//    public JpaTransactionManager transactionManager() {
-//        JpaTransactionManager transactionManager = new JpaTransactionManager();
-//        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-//
-//        return transactionManager;
-//    }
-//
-//    private Properties getHibernateProperties() {
-//        Properties properties = new Properties();
-//        properties.put(PROP_HIBERNATE_DIALECT, env.getRequiredProperty(PROP_HIBERNATE_DIALECT));
-//        properties.put(PROP_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROP_HIBERNATE_SHOW_SQL));
-//        properties.put(PROP_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROP_HIBERNATE_HBM2DDL_AUTO));
-//
-//        return properties;
-//    }
-//
-//
-//
-//
-//
-//    @Bean
-//    @Autowired
-//    public HibernateTemplate hibernateTemplate(SessionFactory sessionFactory) {
-//        return new HibernateTemplate(sessionFactory);
-//    }
-//
-//
-//}
 
-
-import org.hibernate.SessionFactory;
 import org.hibernate.ejb.HibernatePersistence;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTemplate;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 /**
  * @author Olexandr Shevchenko
