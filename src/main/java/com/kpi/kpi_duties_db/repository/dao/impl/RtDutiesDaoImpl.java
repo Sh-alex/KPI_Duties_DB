d8fa32c412a3f44315b6641b1b84935149e6e72d
Shevchenko Olexandr <shevchenko.olexandr96@gmail.com>
2016-12-26 12:22:01 +0200

    changed searchText to filterStr

--- src/main/java/com/kpi/kpi_duties_db/repository/dao/impl/RtDutiesDaoImpl.java
+++ src/main/java/com/kpi/kpi_duties_db/repository/dao/impl/RtDutiesDaoImpl.java
@@ -70,7 +70,7 @@ public class RtDutiesDaoImpl implements RtDutiesDao {
                             if (paramsMap.get("searchType") != null && paramsMap.get("searchType").equals("MATCH_STRING")) {
                                 criteria.add(Restrictions.ilike("name", (String) value, MatchMode.EXACT));
                             } else
-                            if (paramsMap.get("searchType") != null && paramsMap.get("searchType").equals("CONTAINS_STRING") || paramsMap.get("searchType").equals("")) {
+                            if (paramsMap.get("searchType") != null && (paramsMap.get("searchType").equals("CONTAINS_STRING") || paramsMap.get("searchType").equals(""))) {
                                 criteria.add(Restrictions.ilike("name", (String) value, MatchMode.ANYWHERE));
                             }
 
